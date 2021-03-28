38977632eb14d527a3e848d8388ac969c38c006a
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2021-03-28 21:28:57 +0200

    Added simple functionality to MainActivity

--- app/src/main/java/eu/philippleclercq/liboard/MainActivity.kt
+++ app/src/main/java/eu/philippleclercq/liboard/MainActivity.kt
@@ -2,12 +2,23 @@ package eu.philippleclercq.liboard
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 @ExperimentalUnsignedTypes
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LiBoard.GameEventHandler {
+    private lateinit var liBoard: LiBoard
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        liBoard = LiBoard(this, this)
+    }
+
+    override fun onGameStart() {
+        runOnUiThread { textbox.text = liBoard.board.toString() }
+    }
+
+    override fun onMove() {
+        runOnUiThread { textbox.text = liBoard.board.toString() }
     }
 }
