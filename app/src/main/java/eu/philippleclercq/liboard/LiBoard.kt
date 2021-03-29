97cc0aaf57fb4c8e6b93625ffa0aa6b86c9814bb
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2021-03-29 21:59:21 +0200

    Restructured code and added "(Dis)connect" button

--- app/src/main/java/eu/philippleclercq/liboard/LiBoard.kt
+++ app/src/main/java/eu/philippleclercq/liboard/LiBoard.kt
@@ -19,12 +19,50 @@ import java.util.concurrent.Executors
 import kotlin.collections.HashSet
 
 @ExperimentalUnsignedTypes
-internal class LiBoard(activity: Activity, var eventHandler: EventHandler) : Closeable {
-    private val connection = Connection(activity, this)
+internal class LiBoard(private val activity: Activity, var eventHandler: EventHandler) {
     var board = Board()
     private var knownPosition = Position.STARTING_POSITION
     private var physicalPosition = Position.STARTING_POSITION
     private val liftedPieces = HashSet<Int>()
+    private var connection: Connection? = null
+    val isConnected get() = connection != null
+
+    //region Position
+    private class Position(_bytes: Collection<UByte>) {
+        val occupiedSquares: Set<Int>
+        val bytes: List<UByte>
+
+        init {
+            if (_bytes.size != 8)
+                throw LiBoardInvalidPositionError("Position was ${_bytes.size} bytes, expected 8")
+
+            bytes = _bytes.toList()
+
+            val s = HashSet<Int>()
+            _bytes.forEachIndexed { file, byte ->
+                for (rank in 0 until 8) {
+                    if ((byte and (1u shl rank).toUByte()).toInt() != 0) {
+                        s.add(8 * rank + (7 - file))
+                    }
+                }
+            }
+            occupiedSquares = s.toSet()
+        }
+
+        override fun equals(other: Any?): Boolean {
+            return if (other is Position) {
+                bytes == other.bytes
+            } else super.equals(other)
+        }
+
+        override fun hashCode(): Int {
+            return bytes.hashCode()
+        }
+
+        companion object {
+            val STARTING_POSITION = Position(ubyteArrayOf(0xC3U, 0xC3U, 0xC3U, 0xC3U, 0xC3U, 0xC3U, 0xC3U, 0xC3U))
+        }
+    }
 
     private fun makeMove(move: Move): Boolean {
         if (board.doMove(move, true)) {
@@ -89,16 +127,10 @@ internal class LiBoard(activity: Activity, var eventHandler: EventHandler) : Clo
             generateMove()
         }
     }
+    //endregion
 
-    private fun onDisconnect() {
-        eventHandler.onDisconnect()
-    }
-
-    override fun close() {
-        connection.close()
-    }
-
-    private class Connection(val activity: Activity, val liboard: LiBoard) :
+    //region Connection
+    private class Connection(activity: Activity, val liboard: LiBoard) :
         Closeable, SerialInputOutputManager.Listener {
         private val usbManager = activity.getSystemService(Context.USB_SERVICE) as UsbManager
         private val port: UsbSerialPort
@@ -150,8 +182,7 @@ internal class LiBoard(activity: Activity, var eventHandler: EventHandler) : Clo
 
         override fun onRunError(e: java.lang.Exception) {
             Log.e(LOG_TAG, "onRunError")
-            close()
-            liboard.onDisconnect()
+            liboard.disconnect()
         }
 
         companion object {
@@ -161,52 +192,32 @@ internal class LiBoard(activity: Activity, var eventHandler: EventHandler) : Clo
 
     }
 
-    private class Position(_bytes: Collection<UByte>) {
-        val occupiedSquares: Set<Int>
-        val bytes: List<UByte>
-
-        init {
-            if (_bytes.size != 8)
-                throw LiBoardInvalidPositionError("Position was ${_bytes.size} bytes, expected 8")
-
-            bytes = _bytes.toList()
-
-            val s = HashSet<Int>()
-            _bytes.forEachIndexed { file, byte ->
-                for (rank in 0 until 8) {
-                    if ((byte and (1u shl rank).toUByte()).toInt() != 0) {
-                        s.add(8 * rank + (7 - file))
-                    }
-                }
-            }
-            occupiedSquares = s.toSet()
-        }
-
-        override fun equals(other: Any?): Boolean {
-            return if (other is Position) {
-                bytes == other.bytes
-            } else super.equals(other)
-        }
-
-        override fun hashCode(): Int {
-            return bytes.hashCode()
-        }
-
-        companion object {
-            val STARTING_POSITION = Position(ubyteArrayOf(0xC3U, 0xC3U, 0xC3U, 0xC3U, 0xC3U, 0xC3U, 0xC3U, 0xC3U))
-        }
+    fun connect() {
+        if (isConnected) disconnect()
+        connection = Connection(activity, this)
+        eventHandler.onConnect()
     }
 
+    fun disconnect() {
+        connection?.close()
+        connection = null
+        eventHandler.onDisconnect()
+    }
+    //endregion
+
+    //region Interfaces&Exceptions
     internal interface EventHandler {
         fun onGameStart()
         fun onMove()
+        fun onConnect()
         fun onDisconnect()
     }
 
-    internal open class ConnectionException(str: String) : Exception(str)
+    internal abstract class ConnectionException(str: String) : Exception(str)
     internal class MissingDriverException(str: String) : ConnectionException(str)
     internal class UsbPermissionException(str: String) : ConnectionException(str)
     internal class LiBoardInvalidPositionError(str: String) : Exception(str)
+    //endregion
 
     private fun Move(from: Int, to: Int) = Move(Square.squareAt(from), Square.squareAt(to))
 }
--- app/src/main/java/eu/philippleclercq/liboard/MainActivity.kt
+++ app/src/main/java/eu/philippleclercq/liboard/MainActivity.kt
@@ -1,6 +1,7 @@
 package eu.philippleclercq.liboard
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -8,15 +9,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 @ExperimentalUnsignedTypes
 class MainActivity : AppCompatActivity(), LiBoard.EventHandler {
-    private lateinit var liBoard: LiBoard
+    private val liBoard = LiBoard(this, this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        liBoard = LiBoard(this, this)
+        try {
+            liBoard.connect()
+        } catch (e: LiBoard.MissingDriverException) {
+            Toast.makeText(this, "No Board connected", Toast.LENGTH_SHORT).show()
+        } catch (e: LiBoard.UsbPermissionException) {
+            Toast.makeText(this, "No USB Permission", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroy() {
-        liBoard.close()
+        liBoard.disconnect()
         super.onDestroy()
     }
 
@@ -30,7 +37,18 @@ class MainActivity : AppCompatActivity(), LiBoard.EventHandler {
 
     override fun onDisconnect() {
         runOnUiThread {
+            button.text = "Connect"
             Toast.makeText(this, "LiBoard disconnected", Toast.LENGTH_SHORT).show()
         }
     }
+
+    override fun onConnect() {
+        runOnUiThread {
+            button.text = "Disconnect"
+            Toast.makeText(this, "LiBoard connected", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun onButtonPress(view: View) = if (liBoard.isConnected) liBoard.disconnect() else liBoard.connect()
 }
