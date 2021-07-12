8b47f881383207c76eab73bf0dea1f274d3b539a
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2021-07-12 22:51:53 +0200

    Add tests for move generation

---
+++ app/src/test/java/de/pleclercq/liboard/MoveGenTest.kt
@@ -0,0 +1,21 @@
+package de.pleclercq.liboard
+
+import com.github.bhlangonijr.chesslib.Board
+import com.github.bhlangonijr.chesslib.Square
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MoveGenTest {
+    @Test
+    fun moveGenLegalNormal() {
+        val b = Board()
+        assertNotNull(b.findMove(Square.E2, Square.E4))
+    }
+
+    @Test
+    fun moveGenIllegalNormal() {
+        val b = Board()
+        assertNull(b.findMove(Square.A1, Square.E4))
+    }
+}
