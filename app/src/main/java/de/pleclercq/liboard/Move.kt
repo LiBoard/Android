bfaaed20ddac1b103d0e1fa9a8c3f7a144c11dfe
Philipp Leclercq <54592274+PhilLecl@users.noreply.github.com>
2021-07-13 13:10:13 +0200

    Fix bugs related to captures in Move.kt

--- app/src/main/java/de/pleclercq/liboard/Move.kt
+++ app/src/main/java/de/pleclercq/liboard/Move.kt
@@ -1,6 +1,7 @@
 package de.pleclercq.liboard
 
 import com.github.bhlangonijr.chesslib.Board
+import com.github.bhlangonijr.chesslib.Piece
 import com.github.bhlangonijr.chesslib.PieceType
 import com.github.bhlangonijr.chesslib.Square
 import com.github.bhlangonijr.chesslib.move.Move
@@ -8,12 +9,13 @@ import kotlin.math.abs
 
 fun Board.isCapture(move: Move) = isNormalCapture(move) || isEnPassant(move)
 
-fun Board.isNormalCapture(move: Move) = getPiece(move.to).pieceType == PieceType.NONE
+fun Board.isNormalCapture(move: Move) = getPiece(move.to) != Piece.NONE
 
+// TODO implement 960 castling
 fun Board.isCastling(move: Move) =
     getPiece(move.from).pieceType == PieceType.KING && abs(move.from.ordinal - move.to.ordinal) == 2
 
-fun Board.isEnPassant(move: Move) = getPiece(move.from).pieceType == PieceType.PAWN && enPassantTarget == move.to
+fun Board.isEnPassant(move: Move) = getPiece(move.from).pieceType == PieceType.PAWN && enPassant == move.to
 
 fun Board.findMove(from: Square, to: Square) = legalMoves().firstOrNull { m -> m.from == from && m.to == to }
 fun Board.findMove(from: Int, to: Int) = findMove(Square.squareAt(from), Square.squareAt(to))
